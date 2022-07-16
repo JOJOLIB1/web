30e680b87b064bf0d029514eb87eb91eda3e3f3b
江骏杰 <a6691893@163.com>
2022-07-16 22:51:57 +0800

    httpServletRequest preparing

---
+++ study/src/http/RequestTestServlet.java
@@ -0,0 +1,21 @@
+package http;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * @className: http.RequestTestServlet
+ * @description:
+ * @author: 江骏杰
+ * @create: 2022-07-16 21:41
+ */
+public class RequestTestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
